ea100960aeba285c5374fff5f075ca6aad87b10d
ramesh1290 <krrishchhetri6@gmail.com>
2026-03-16 19:21:53 +0545

    setting.py fixed

--- backend/chatproject/chatproject/asgi.py
+++ backend/chatproject/chatproject/asgi.py
@@ -8,15 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chatapp.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
 
+django_asgi_app = get_asgi_application()
+
+import chatapp.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chatapp.routing.websocket_urlpatterns
